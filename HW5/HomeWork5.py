568c15e1a9ab4b6133d4da579dda6526553ea8d4
AlexeyLopuxov <alexeylopyxov00@gmail.com>
2023-03-31 02:56:45 +0500

    Home Work 5

---
+++ HW5/HomeWork5.py
@@ -0,0 +1,28 @@
+# Задача 26
+# Возвести число А в степень В
+
+# a = int(input('Введите основание степени: '))
+# b = int(input('Введите показатель степени: '))
+
+# def degree(a, b):
+#     if b == 0:
+#         return 1
+#     return a * degree(a, b-1)
+
+# print(degree(a,b))
+
+
+
+
+# Задача 28
+# Сумма двух чисел
+
+# a = int(input('Введите первое число: '))
+# b = int(input('Введите второе число: '))
+
+# def summa(a, b):
+#     if b == 0:
+#         return a
+#     return summa(a, b - 1) + 1
+
+# print(summa(a, b))
